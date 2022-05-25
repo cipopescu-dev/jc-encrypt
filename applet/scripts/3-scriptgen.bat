6ca5754ae81f57196db155abb08972eb4baa155e
popescuciprian <popescu.ciprian98@gmail.com>
2022-05-25 09:05:25 +0300

    reset main

--- applet/scripts/3-scriptgen.bat
+++
@@ -1,3 +0,0 @@
-scriptgen.bat ^
--hashfile apdu-scripts/digest.txt ^
--o apdu-scripts/cap.script out/production/applet/com/cipdev/jc/javacard/jc.cap
--- host-app/src/com/cipdev/jcapp/App.java
+++
@@ -1,7 +0,0 @@
-package com.cipdev.jcapp;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
