5741ee043c7297eaacf71ff54356950e77eede72
popescuciprian <popescu.ciprian98@gmail.com>
2022-05-28 06:08:10 +0300

    APPLET

---
+++ AesApplet/src/com/cipdev/jc/AesApplet.java
@@ -0,0 +1,170 @@
+package com.cipdev.jc;
+
+import javacard.framework.*;
+import javacard.security.AESKey;
+import javacard.security.KeyBuilder;
+import javacardx.annotations.*;
+import javacardx.crypto.Cipher;
+
+import static com.cipdev.jc.AesAppletStrings.*;
+
+/**
+ * Applet class
+ * 
+ * @author Stefan-Ciprian Popescu
+ *
+ */
+@StringPool(value = {
+        @StringDef(name = "Package", value = "com.cipdev.jc"),
+        @StringDef(name = "AppletName", value = "AesApplet") }, name = "AesAppletStrings")
+public class AesApplet extends Applet {
+    final static byte CLA_AES_APPLET = (byte) 0x80;
+
+    final static byte INS_VERIFY = (byte) 0x20;
+    final static byte INS_INIT = (byte) 0x30;
+    final static byte INS_UPDATE = (byte) 0x40;
+    final static byte INS_DOFINAL = (byte) 0x50;
+
+    final static byte P1_MODE_ENCRYPT_CIPHER = (byte) 0x01;
+    final static byte P1_MODE_DECRYPT_CIPHER = (byte) 0x02;
+
+    final static short SW_PIN_VERIFICATION_FAILED = 0x6300;
+    final static short SW_PIN_VERIFICATION_REQUIRED = 0x6301;
+    final static short SW_CIPHER_NOMODESET = 0x6302;
+    final static short SW_CIPHER_BLOCK_NOALLIGN = 0x6303;
+    final static short SW_CIPHER_BLOCK_NEEDUPDATE = 0x6304;
+    final static short SW_CIPHER_BLOCK_OVERFLOW = 0x6305;
+
+    final static byte PIN_MAX_SIZE = (byte) 0x0A;
+    final static byte PIN_TRY_LIMIT = (byte) 0x03;
+    final static byte CIPHER_MAX_BLOCK_SIZE = (byte) 0x80;
+    final static short CIPHER_CHUNK_SIZE = 0x0010;
+
+    OwnerPIN pin;
+    AESKey key;
+    Cipher cipher;
+    byte cipherMode;
+
+    public static void install(byte[] bArray, short bOffset, byte bLength) {
+        new AesApplet(bArray, bOffset);
+    }
+
+    protected AesApplet(byte[] bArray, short bOffset) {
+        bOffset -= 1;
+        pin = new OwnerPIN(PIN_TRY_LIMIT, PIN_MAX_SIZE);
+        byte pinLength = bArray[bOffset++];
+        pin.update(bArray, bOffset, pinLength);
+
+        bOffset += pinLength;
+
+        key = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, true);
+        key.setKey(bArray, ++bOffset);
+
+        cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+        cipherMode = 0x00;
+        register();
+    }
+
+    @Override
+    public void process(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        buffer[ISO7816.OFFSET_CLA] = (byte) (buffer[ISO7816.OFFSET_CLA] & (byte) 0xFC);
+        if ((buffer[ISO7816.OFFSET_CLA] == 0) && (buffer[ISO7816.OFFSET_INS] == (byte) (0xA4)))
+            return;
+        if (buffer[ISO7816.OFFSET_CLA] != CLA_AES_APPLET)
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        switch (buffer[ISO7816.OFFSET_INS]) {
+            case INS_INIT:
+                init(apdu);
+                return;
+            case INS_UPDATE:
+                update(apdu);
+                return;
+            case INS_DOFINAL:
+                dofinal(apdu);
+                return;
+            case INS_VERIFY:
+                verify(apdu);
+                return;
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        }
+    }
+
+    @Override
+    public boolean select() {
+        if (pin.getTriesRemaining() == 0)
+            return false;
+        return true;
+    }
+
+    @Override
+    public void deselect() {
+        pin.reset();
+        cipherMode = 0x00;
+    }
+
+    private void init(APDU apdu) {
+        if (!pin.isValidated())
+            ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+        byte[] buffer = apdu.getBuffer();
+        switch (buffer[ISO7816.OFFSET_P1]) {
+            case P1_MODE_ENCRYPT_CIPHER:
+                cipher.init(key, Cipher.MODE_ENCRYPT);
+                cipherMode = Cipher.MODE_ENCRYPT;
+                return;
+            case P1_MODE_DECRYPT_CIPHER:
+                cipher.init(key, Cipher.MODE_DECRYPT);
+                cipherMode = Cipher.MODE_DECRYPT;
+                return;
+            default:
+                ISOException.throwIt(ISO7816.SW_INCORRECT_P1P2);
+        }
+
+    }
+
+    private void update(APDU apdu) {
+        if (!pin.isValidated())
+            ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+        if (cipherMode == 0)
+            ISOException.throwIt(SW_CIPHER_NOMODESET);
+        byte[] buffer = apdu.getBuffer();
+        short dataSize = apdu.setIncomingAndReceive();
+        if (dataSize % CIPHER_CHUNK_SIZE != 0)
+            ISOException.throwIt(SW_CIPHER_BLOCK_NOALLIGN);
+        if (dataSize > CIPHER_MAX_BLOCK_SIZE)
+            ISOException.throwIt(SW_CIPHER_BLOCK_OVERFLOW);
+        for (short i = 0; i < dataSize / CIPHER_CHUNK_SIZE; i++) {
+            cipher.update(buffer, (short) (i * CIPHER_CHUNK_SIZE + ISO7816.OFFSET_CDATA), (short) CIPHER_CHUNK_SIZE,
+                    buffer,
+                    (short) (i * CIPHER_CHUNK_SIZE + ISO7816.OFFSET_CDATA));
+        }
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, dataSize);
+
+    }
+
+    private void dofinal(APDU apdu) {
+        if (!pin.isValidated())
+            ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+        if (cipherMode == 0)
+            ISOException.throwIt(SW_CIPHER_NOMODESET);
+        byte[] buffer = apdu.getBuffer();
+        byte dataSize = (byte) (apdu.setIncomingAndReceive());
+        if (dataSize > 16)
+            ISOException.throwIt(SW_CIPHER_BLOCK_NEEDUPDATE);
+
+        for (short i = (short) (ISO7816.OFFSET_CDATA + dataSize); i < dataSize % CIPHER_CHUNK_SIZE
+                + ISO7816.OFFSET_CDATA + dataSize; i++)
+            buffer[i] = 0x00;
+        buffer[ISO7816.OFFSET_CDATA + CIPHER_CHUNK_SIZE] = 0x7F;
+        cipher.doFinal(buffer, ISO7816.OFFSET_CDATA, (short) CIPHER_CHUNK_SIZE, buffer, ISO7816.OFFSET_CDATA);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, dataSize);
+    }
+
+    private void verify(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte byteRead = (byte) (apdu.setIncomingAndReceive());
+        if (pin.check(buffer, ISO7816.OFFSET_CDATA, byteRead) == false)
+            ISOException.throwIt(SW_PIN_VERIFICATION_FAILED);
+    }
+}
--- README.md
+++ README.md
@@ -4,22 +4,28 @@
 * `JC_HOME` variable set, on a folder containing both the tools and simulator (just extract jc tools over jc simulator)
 * JDK 17 (note: target is `java 1.7` for the Applet, since language level 17 is not supported yet.)
 * ClassPath set with `$JC_HOME/lib/*` 
-* Custom `.vscode\settings.json` template:
+* Custom `.vscode\settings.json` template to ignore classpath errors in vscode
   ```JSON
   {
     "java.project.referencedLibraries": [
         "c:\\Users\\cipdev\\jc310\\lib\\*" 
     ],
     "java.project.sourcePaths": [
-        "applet/src",
-        "host-app/src/"
+        "AesApplet/src"
     ]
   }
   ```
 
 ---
 ## Applet
-* You should run every script from the `applet` directory
+It sets it's password and AES key during the creation process, using the `CREATE` apdu. Checkout [the APDU examples](AesApplet\apdu_scripts\examples.script)
+
+There are 4 main instructions:
+1. Initialization  of the cipher (encrypt / decrypt)
+2. Cipher `.update(..)` callable multiple times for a maximum block size of 128Bytes. 
+3. Cipher `.doFinal(..)` used **only** for the last 16B (or less) of the file
+4. Validation of password 
+
+> Padding notice: Since Oracle card Simulator does not support any kind of padding for `AES-CBC`, the applet will append `0x00`s to the last set of bytes until it reach the 16-byte block size required for an `AES128` key.  We can easily store the initial size of the message at the start (or end) of the encrypted message.
 ---
 ## Host App
-* You should run every script from the `host-app` directory
